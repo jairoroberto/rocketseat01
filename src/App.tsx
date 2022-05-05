6dfd049c4cc85f0ee546888e0f2283a51c0b5c92
Jairo Roberto <jairorob@gmail.com>
2022-05-05 08:23:43 -0300

    Versão Inicial Diego Rocket II - Conceitos Básicos

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function Button(props: ButtonProps) {
 function App() {
     //return <h1>Hello World</h1>
     return (
-        <div className='p-5 flex gap-8'>
+        <div className='bg-slate-50 justify-evenly items-center rounded-md shadow-lg ring-1 ring-slate-900/5 max-w-2xl mt-20 h-80 mx-auto p-5 flex flex-align-center gap-8 hover:bg-sky-100 hover:ring-sky-500'>
             <Button text='Enviar'/>
             <Button text='OK'/>
             <Button />
