32e15014137bb0e136ed23304d8ed856f2316791
Jairo Roberto <jairorob@gmail.com>
2022-05-05 08:50:06 -0300

    Adjust code to Project - And Icon Phospher

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
 import { useState } from 'react'
+import { Widget } from './components/Widget'
 
-// Components 01
-/*
-function Button(){
-    const [count, setCount] = useState(0)
-    return (
-        <button onClick={
-          () => setCount(count + 1)
-          }>
-            Você clicou {count} vezes
-        </button>
-    )
-}
-*/
-
-// TIPANDO 
-interface ButtonProps {
-  text?: string,
+export function App() {
+    return <Widget /> 
 }
 
-// Components 01
-function Button(props: ButtonProps) {
-    return <button className="btn01">{props.text ?? 'Default'}</button>
-}
-
-function App() {
-    //return <h1>Hello World</h1>
-    return (
-        <div className='bg-slate-50 justify-evenly items-center rounded-md shadow-lg ring-1 ring-slate-900/5 max-w-2xl mt-20 h-80 mx-auto p-5 flex flex-align-center gap-8 hover:bg-sky-100 hover:ring-sky-500'>
-            <Button text='Enviar'/>
-            <Button text='OK'/>
-            <Button />
-        </div>
-    )
-} //JSX TSX
-
-export default App
---
+++ src/components/Widget.tsx
@@ -0,0 +1,9 @@
+import {ChatTeardropDots} from 'phosphor-react';
+
+export function Widget(){
+    return (
+        <button>
+            <ChatTeardropDots className='w-6' size='100' weight='duotone'/>
+        </button>
+    );
+}
