9725df93f4f8f66c3f7d8728975d6df212a4468d
Jairo Roberto <jairorob@gmail.com>
2022-05-05 09:24:17 -0300

    Funções de React/JS

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,28 @@
 import {ChatTeardropDots} from 'phosphor-react';
+import { useState } from 'react';
+
+//Estado 
 
 export function Widget(){
+
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen);
+    }
+
     return (
-        <button>
-            <ChatTeardropDots className='w-6' size='100' weight='duotone'/>
-        </button>
+        <div className='absolute right-5 bottom-5'>
+
+            { isWidgetOpen &&  <p >Hello World</p> }
+           
+            <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+                <ChatTeardropDots className='w-6 h-6' weight='bold'/>
+                <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+                    <span className='pl-2'></span>
+                    Feedback
+                </span>
+            </button>
+        </div>
     );
 }
